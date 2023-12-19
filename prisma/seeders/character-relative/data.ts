6d2292e5dab8e636a02896aec14f8a94fde4bc11
dehsilva <andre.silva.clinica@gmail.com>
2023-12-19 12:00:17 -0300

    Adding use of enum RelationshipTypeEnum on ther seeder for the entity character_relative

--- prisma/seeders/character-relative/data.ts
+++ prisma/seeders/character-relative/data.ts
@@ -1,10 +1,34 @@
+import { RelationshipTypeEnum } from '../../../src/modules/common/enums/relationship-type.enum';
+
 export const characterRelativeSeederData = [
   {
     characterId: 1, // Spider-Man
     relativeId: 2, // Spider-Girl
+    relationshipTypeId: RelationshipTypeEnum.daughter,
+  },
+  {
+    characterId: 2, // Spider-Girl
+    relativeId: 1, // Spider-Man
+    relationshipTypeId: RelationshipTypeEnum.father,
   },
   {
     characterId: 19, // Invisible Woman
     relativeId: 21, // Human Torch
+    relationshipTypeId: RelationshipTypeEnum.brother
+  },
+  {
+    characterId: 21, // Human Torch
+    relativeId: 19, // Invisible Woman
+    relationshipTypeId: RelationshipTypeEnum.sister
+  },
+  {
+    characterId: 19, // Invisible Woman
+    relativeId: 20, // Mister Fantastic
+    relationshipTypeId: RelationshipTypeEnum.husband
+  },
+  {
+    characterId: 20, // Mister Fantastic
+    relativeId: 19, // Invisible Woman
+    relationshipTypeId: RelationshipTypeEnum.wife
   },
 ];
