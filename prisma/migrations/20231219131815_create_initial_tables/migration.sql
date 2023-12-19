4075040a98ce3001893994ed3eb4d8228faa4af7
dehsilva <andre.silva.clinica@gmail.com>
2023-12-19 10:22:51 -0300

    Adding table relationship_type on migration file.

--- prisma/migrations/20231219131815_create_initial_tables/migration.sql
+++ prisma/migrations/20231219131815_create_initial_tables/migration.sql
@@ -11,24 +11,24 @@ CREATE TABLE `character` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(100) NOT NULL,
     `civilName` VARCHAR(100) NULL,
-    `gender` ENUM('MALE', 'FEMALE') NOT NULL,
-    `alignmentTypeId` INTEGER NOT NULL,
-    `maritalStatusTypeId` INTEGER NOT NULL,
-    `livingStatusTypeId` INTEGER NOT NULL,
+    `gender` ENUM('male', 'female') NOT NULL,
+    `alignmentId` INTEGER NOT NULL,
+    `maritalStatusId` INTEGER NOT NULL,
+    `livingStatusId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    INDEX `character_alignmentTypeId_idx`(`alignmentTypeId`),
-    INDEX `character_maritalStatusTypeId_idx`(`maritalStatusTypeId`),
-    INDEX `character_livingStatusTypeId_idx`(`livingStatusTypeId`),
+    INDEX `character_alignmentId_idx`(`alignmentId`),
+    INDEX `character_maritalStatusId_idx`(`maritalStatusId`),
+    INDEX `character_livingStatusId_idx`(`livingStatusId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `character_ally` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `characterId` INTEGER NULL,
-    `allyId` INTEGER NULL,
+    `characterId` INTEGER NOT NULL,
+    `allyId` INTEGER NOT NULL,
     `addedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     INDEX `character_ally_characterId_idx`(`characterId`),
@@ -39,8 +39,8 @@ CREATE TABLE `character_ally` (
 -- CreateTable
 CREATE TABLE `character_enemy` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `characterId` INTEGER NULL,
-    `enemyId` INTEGER NULL,
+    `characterId` INTEGER NOT NULL,
+    `enemyId` INTEGER NOT NULL,
     `addedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     INDEX `character_enemy_characterId_idx`(`characterId`),
@@ -51,12 +51,14 @@ CREATE TABLE `character_enemy` (
 -- CreateTable
 CREATE TABLE `character_relative` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `characterId` INTEGER NULL,
-    `relativeId` INTEGER NULL,
+    `characterId` INTEGER NOT NULL,
+    `relativeId` INTEGER NOT NULL,
+    `relationshipTypeId` INTEGER NOT NULL,
     `addedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     INDEX `character_relative_characterId_idx`(`characterId`),
     INDEX `character_relative_relativeId_idx`(`relativeId`),
+    INDEX `character_relative_relationshipTypeId_idx`(`relationshipTypeId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -73,8 +75,8 @@ CREATE TABLE `character_power` (
 CREATE TABLE `character_team` (
     `characterId` INTEGER NOT NULL,
     `teamId` INTEGER NOT NULL,
-    `status` ENUM('CURRENT', 'FORMER') NOT NULL,
-    `role` ENUM('LEADER', 'MEMBER') NOT NULL,
+    `status` ENUM('current', 'former') NOT NULL,
+    `role` ENUM('leader', 'member') NOT NULL,
     `addedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`characterId`, `teamId`)
@@ -107,6 +109,14 @@ CREATE TABLE `power` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `relationship_type` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `team` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(100) NOT NULL,
@@ -117,31 +127,34 @@ CREATE TABLE `team` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `character` ADD CONSTRAINT `character_alignmentTypeId_fkey` FOREIGN KEY (`alignmentTypeId`) REFERENCES `alignment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `character` ADD CONSTRAINT `character_alignmentId_fkey` FOREIGN KEY (`alignmentId`) REFERENCES `alignment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `character` ADD CONSTRAINT `character_maritalStatusTypeId_fkey` FOREIGN KEY (`maritalStatusTypeId`) REFERENCES `marital_status`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `character` ADD CONSTRAINT `character_maritalStatusId_fkey` FOREIGN KEY (`maritalStatusId`) REFERENCES `marital_status`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `character` ADD CONSTRAINT `character_livingStatusTypeId_fkey` FOREIGN KEY (`livingStatusTypeId`) REFERENCES `living_status`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `character` ADD CONSTRAINT `character_livingStatusId_fkey` FOREIGN KEY (`livingStatusId`) REFERENCES `living_status`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `character_ally` ADD CONSTRAINT `character_ally_characterId_fkey` FOREIGN KEY (`characterId`) REFERENCES `character`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `character_ally` ADD CONSTRAINT `character_ally_characterId_fkey` FOREIGN KEY (`characterId`) REFERENCES `character`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `character_ally` ADD CONSTRAINT `character_ally_allyId_fkey` FOREIGN KEY (`allyId`) REFERENCES `character`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `character_ally` ADD CONSTRAINT `character_ally_allyId_fkey` FOREIGN KEY (`allyId`) REFERENCES `character`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `character_enemy` ADD CONSTRAINT `character_enemy_characterId_fkey` FOREIGN KEY (`characterId`) REFERENCES `character`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `character_enemy` ADD CONSTRAINT `character_enemy_characterId_fkey` FOREIGN KEY (`characterId`) REFERENCES `character`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `character_enemy` ADD CONSTRAINT `character_enemy_enemyId_fkey` FOREIGN KEY (`enemyId`) REFERENCES `character`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `character_enemy` ADD CONSTRAINT `character_enemy_enemyId_fkey` FOREIGN KEY (`enemyId`) REFERENCES `character`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `character_relative` ADD CONSTRAINT `character_relative_characterId_fkey` FOREIGN KEY (`characterId`) REFERENCES `character`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `character_relative` ADD CONSTRAINT `character_relative_characterId_fkey` FOREIGN KEY (`characterId`) REFERENCES `character`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `character_relative` ADD CONSTRAINT `character_relative_relativeId_fkey` FOREIGN KEY (`relativeId`) REFERENCES `character`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `character_relative` ADD CONSTRAINT `character_relative_relativeId_fkey` FOREIGN KEY (`relativeId`) REFERENCES `character`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `character_relative` ADD CONSTRAINT `character_relative_relationshipTypeId_fkey` FOREIGN KEY (`relationshipTypeId`) REFERENCES `relationship_type`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `character_power` ADD CONSTRAINT `character_power_characterId_fkey` FOREIGN KEY (`characterId`) REFERENCES `character`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
